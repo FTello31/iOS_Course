c0f80855cd331a2655b3c5970655407e7ec00134
Fernando Tello <fernando@Fernandos-MacBook-Pro-2.local>
2022-02-13 11:23:57 -0500

    17 module finished

---
+++ 3-I-am-Poor-challenge/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Fernando on 11/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
